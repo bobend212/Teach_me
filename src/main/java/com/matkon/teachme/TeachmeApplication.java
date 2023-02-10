045a971ab5adc40f17c73716fb5e0eafdcad4fcc
Mateusz Konopka <mkonopkadev@gmail.com>
2023-02-10 20:16:44 +0100

    initial

---
+++ src/main/java/com/matkon/teachme/TeachmeApplication.java
@@ -0,0 +1,13 @@
+package com.matkon.teachme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachmeApplication.class, args);
+	}
+
+}
